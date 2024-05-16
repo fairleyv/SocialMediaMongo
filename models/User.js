b2a88d39cbb27afa1ddf769d8374b91b8521a522
fairleyv <fairleyvincent@gmail.com>
2024-05-15 18:09:01 -0700

    finished users model and created friendsCount virtual

--- models/User.js
+++ models/User.js
@@ -3,32 +3,40 @@ const { Schema, model} = require('mongoose');
 // Schema to create user model
 
 const userSchema = new Schema({
+    // username(string,unique,required,trimmed)
     username: {
-        // username(string,unique,required,trimmed)
         type: String,
         required: true,
         unique: true,
         trim: true,
     },
+    // email(string,required,unique,validate(email))
     email: {
-        // email(string,required,unique,validate(email))
         type: String,
         required: true,
         lowercase: true,
         validate: {
             validator: function(email) {
                 return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-            }
+            },
+            message: 'Please enter a valid email address'
         }
     },
-    thoughts: {
-        // thoughts (array of _id values referencing the thought model)
-        thoughts: []
-    },
-    friends: {
-        // array of _id values referncing the user model (self-reference)
-    }
-}
-);
-
+    // thoughts (array of _id values referencing the thought model)
+    thoughts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    // array of _id values referncing the user model (self-reference)
+    friends: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+});
 // create a friendCount virtual that retrieves the length of the user's friends array field on query
+userSchema.virtual('friendCount').get(function(){
+    return this.friends.length
+});
+
+//  Create User Model
+const User = mongoose.model('User', userSchema);
